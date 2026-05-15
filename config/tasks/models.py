d5b2c4074c471f99d09af418f168b01164edeffa
murakasi <murakasi.ruslan@gmail.com>
2026-05-15 10:54:07 +0200

    add model Task

--- config/tasks/models.py
+++ config/tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from projects.models import Project
 
-# Create your models here.
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['project', 'title'],
+                name='unique_title_per_project'
+            )
+        ]
