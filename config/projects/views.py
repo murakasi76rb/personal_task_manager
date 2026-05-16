6bc0f4f6987f3a4b77ad91987fcdab1ab2013ef4
murakasi <murakasi.ruslan@gmail.com>
2026-05-16 12:00:30 +0200

    add CRUD projects/views.py

--- config/projects/views.py
+++ config/projects/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest
+from projects.models import Project
+from projects.forms import ProjectForm
 
-# Create your views here.
+
+def project_list(request: HttpRequest):
+    projects = Project.objects.filter(owner=request.user)
+    context = {
+        'projects': projects
+    }
+    return render(request, 'projects/list.html', context)
+
+
+def project_create(request:HttpRequest):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, owner=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('projects:list')
+    else:
+        form = ProjectForm(owner=request.user)
+    context = {
+            'form': form
+    }
+    return render(request, 'projects/create.html', context)
+
+
+def project_detail(request:HttpRequest, project_id:int):
+    project = get_object_or_404(Project, pk=project_id, owner=request.user)
+    tasks = project.tasks.all() #type: ignore
+    context = {
+        'project': project,
+        'tasks': tasks
+    }
+
+    return render(request, 'projects/detail.html', context)
+
+
+def projects_update(request:HttpRequest, project_id:int):
+    project = get_object_or_404(Project, pk=project_id, owner=request.user)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project, owner=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('projects:list')
+    else:
+        form = ProjectForm(instance=project, owner=request.user)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/update.html', context)
+
+def project_delete(request:HttpRequest, project_id:int):
+    project = get_object_or_404(Project, pk=project_id, owner=request.user)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects:list')
+    context = {
+        'project': project
+    }
+    return render(request, 'projects/delete.html', context)
+
