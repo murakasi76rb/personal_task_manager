139266fe50003c58513ad8393ef29765d4cb1925
murakasi <murakasi.ruslan@gmail.com>
2026-05-18 22:05:58 +0200

    add tasks/create.html and path for him

--- config/tasks/urls.py
+++ config/tasks/urls.py
@@ -4,6 +4,7 @@ from tasks import views
 app_name = "tasks"
 
 urlpatterns = [
+    path('create-task/', views.task_create, name='create'),
     path('<int:task_id>/detail/', views.task_detail, name='detail'),
     path('<int:task_id>/update/', views.task_update, name='update'),
 ]
