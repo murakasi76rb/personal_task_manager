741029db21d16cfb76fcb55023ba328dd321188b
murakasi <murakasi.ruslan@gmail.com>
2026-05-18 22:23:46 +0200

    add tasks/delete.html and path in tasks/urls.py

--- config/tasks/urls.py
+++ config/tasks/urls.py
@@ -4,7 +4,8 @@ from tasks import views
 app_name = "tasks"
 
 urlpatterns = [
-    path('create-task/', views.task_create, name='create'),
+    path('project/<int:project_id>/create-task/', views.task_create, name='create'),
     path('<int:task_id>/detail/', views.task_detail, name='detail'),
     path('<int:task_id>/update/', views.task_update, name='update'),
+    path("<int:task_id>/delete/", views.task_delete, name='delete'),
 ]
